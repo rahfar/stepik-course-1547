8b7e0f8737e9b7d5511aafadddf93eb0a256c762
rahfar <rahfar777@gmail.com>
2022-02-07 12:50:34 +0300

    1.2.2 - add test case

--- 1.2.2/test_main.py
+++ 1.2.2/test_main.py
@@ -11,6 +11,7 @@ from main import main
         ("1\n-1\n", "1\n"),
         ("5\n4 -1 4 1 1\n", "3\n"),
         ("5\n-1 0 4 0 3\n", "4\n"),
+        ("10\n9 7 5 5 2 9 9 9 2 -1\n", "4\n"),
     ],
 )
 def test_main(monkeypatch, capsys, test_input, expected):
